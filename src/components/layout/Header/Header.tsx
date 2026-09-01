768da7235766e0af8a8f1ac2b6f24942fe798a8a
Vadym Kirilkov <tav0r0tuk@gmail.com>
2026-09-01 16:51:22 +0200

    style(layout): introduce global spacing variables and apply to layout components

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,13 +1,23 @@
 import { Logo } from '@/components/ui/Logo';
 import { HeaderNavigation } from '@/components/layout/Header/HeaderNavigation';
 import { HeaderActions } from '@/components/layout/Header/HeaderActions';
+import { useLocation } from 'react-router-dom';
 
 export const Header = () => {
+  const { pathname } = useLocation();
+  const isHomePage = pathname === '/';
+
   return (
-    <header className="bg-primary flex justify-between items-center px-[48px] pt-[38px]">
-      <Logo />
-      <HeaderNavigation />
-      <HeaderActions />
+    <header className="bg-primary grid grid-cols-3 items-center px-global pt-header">
+      <div className="justify-self-start min-h-[64px]">
+        {!isHomePage && <Logo />}
+      </div>
+      <div className="justify-self-center">
+        <HeaderNavigation />
+      </div>
+      <div className="justify-self-end">
+        {!isHomePage && <HeaderActions />}
+      </div>
     </header>
-  );
+  )
 };
