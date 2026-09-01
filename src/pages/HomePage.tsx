ef621fe5b4d1b52ea02c835f0daede7df225c4b3
Vadym Kirilkov <tav0r0tuk@gmail.com>
2026-09-01 16:53:39 +0200

    feat(ui): integrate HeroLogo and configure header-to-home transitions

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,9 @@
+import { HeroLogo } from "@/components/ui/HeroLogo"
+
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
+    <div className="bg-primary min-h-[calc(100vh-102px)] relative w-full overflow-hidden">
+      <HeroLogo />
+    </div>
+  );
 }
