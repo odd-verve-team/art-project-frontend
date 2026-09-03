fd2f7984701de05715a30661178021f155262772
Vadym Kirilkov <tav0r0tuk@gmail.com>
2026-09-03 20:03:48 +0200

    feat(ui): create AboutSection component and connect to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,19 @@
+import { AboutSection } from "@/components/features/AboutSection";
 import { HeroSlider } from "@/components/features/HeroSlider/HeroSlider";
 import { HeroFrame } from "@/components/ui/HeroFrame";
 import { HeroLogo } from "@/components/ui/HeroLogo"
 
 export const HomePage = () => {
   return (
-    <div className="bg-primary min-h-[calc(100vh-102px)] relative w-full overflow-hidden">
-      <HeroLogo />
-      <HeroFrame />
-      <HeroSlider />
+    <div className="w-full">
+      <div className="bg-primary min-h-[calc(100vh-102px)] relative overflow-hidden">
+        <HeroLogo />
+        <HeroFrame />
+        <HeroSlider />
+      </div>
+      <div className="mx-global">
+        <AboutSection />
+      </div>
     </div>
   );
 }
