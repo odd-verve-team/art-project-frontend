062462df993e09d698e52a64834a08afc108351e
Vadym Kirilkov <tav0r0tuk@gmail.com>
2026-09-06 20:35:54 +0200

    chore(gallery): define SORT_OPTIONS constants for sorting dropdown

---
+++ src/components/features/Gallery/galleryConstants.ts
@@ -0,0 +1,10 @@
+export const SORT_OPTIONS = [
+  { label: 'PRICE : LOW TO HIGH', value: 'price_asc' },
+  { label: 'PRICE : HIGH TO LOW', value: 'price_desc' },
+  { label: 'DATE : NEWEST ARRIVALS', value: 'date_desc' },
+  { label: 'DATE : OLDEST', value: 'date_asc' },
+  { label: 'YEAR OF CREATION : NEW TO OLD', value: 'year_desc' },
+  { label: 'YEAR OF CREATION : OLD TO NEW', value: 'year_asc' },
+  { label: 'SIZE: SMALL TO LARGE', value: 'size_asc' },
+  { label: 'SIZE: LARGE TO SMALL', value: 'size_desc' },
+];
