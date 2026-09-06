8200c2b65854e329252db1ba71a8a4fd67a317d0
Vadym Kirilkov <tav0r0tuk@gmail.com>
2026-09-06 20:00:48 +0200

    feat(ui): integrate ArtworkGrid and header into GalleryPage

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -1,3 +1,28 @@
+import { ArtworkGrid } from '@/components/features/Artworks/ArtworkGrid';
+import { useArtworkStore } from '@/store/useArtworkStore';
+import { useEffect } from 'react';
+
 export const GalleryPage = () => {
-  return <div>GalleryPage</div>;
+  const galleryArtworks = useArtworkStore((state) => state.galleryArtworks);
+  const fetchGalleryArtworks = useArtworkStore(
+    (state) => state.fetchGalleryArtworks,
+  );
+
+  useEffect(() => {
+    fetchGalleryArtworks({ status: 'approved' });
+  }, [fetchGalleryArtworks]);
+
+  return (
+    <div className="">
+      <h2
+        className={`
+        bg-primary pt-[80px] pb-[110px] text-center
+        text-background text-[140px] font-[700] uppercase
+      `}
+      >
+        gallery
+      </h2>
+      <ArtworkGrid artworks={galleryArtworks} />
+    </div>
+  );
 };
