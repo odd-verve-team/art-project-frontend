331f390222ea689024417731683ee6078e91f2c9
Vadym Kirilkov <tav0r0tuk@gmail.com>
2026-09-12 18:13:06 +0200

    feat(ui): implement responsive layout for ArtworkCard component

--- src/components/features/Artworks/ArtworkCard.tsx
+++ src/components/features/Artworks/ArtworkCard.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import type { Artwork } from "@/types/artwork";
-import HeartIcon from "@/assets/heart-icon.svg";
+import type { Artwork } from '@/types/artwork';
+import HeartIcon from '@/assets/heart-icon.svg';
 
 export const ArtworkCard = ({ artwork }: { artwork: Artwork }) => {
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const size = `${artwork.image_width} / ${artwork.image_height}`;
   const authorName = `${artwork.artist.first_name} ${artwork.artist.last_name}`;
@@ -19,19 +19,20 @@ export const ArtworkCard = ({ artwork }: { artwork: Artwork }) => {
         alt={artwork.title}
         onLoad={() => setIsLoaded(true)}
         className={`
-          absolute inset-0 w-full h-full object-cover
-          p-[24px] border-[1px] border-primary
-          transition-opacity duration-700 ease-in-out
+          absolute inset-0 w-full h-full object-cover border-[1px] border-primary transition-opacity duration-700 ease-in-out
+          p-[8px]
+          md:p-[16px]
+          lg:p-[24px]
           ${isLoaded ? 'opacity-100' : 'opacity-0'}
         `}
       />
 
       <div
         className={`
-          absolute inset-0
-          bg-gradient-to-t from-primary/60 via-primary/50 to-primary/40
-          opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out
-          flex flex-col justify-end p-[24px] pointer-events-none
+          absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-primary/60 via-primary/50 to-primary/40 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out pointer-events-none
+          p-[8px]
+          md:p-[16px]
+          lg:p-[24px]
         `}
       >
         <div className="w-full flex items-center justify-between pointer-events-auto">
@@ -43,8 +44,8 @@ export const ArtworkCard = ({ artwork }: { artwork: Artwork }) => {
             />
             <span
               className={`
-              text-primary text-[12px] font-[700]
-              leading-none tracking-wide uppercase`}
+                text-primary text-[12px] font-[700] leading-none tracking-wide uppercase
+              `}
             >
               {authorName}
             </span>
@@ -52,7 +53,7 @@ export const ArtworkCard = ({ artwork }: { artwork: Artwork }) => {
           <button
             type="button"
             onClick={(e) => e.stopPropagation()}
-            className="p-[5px] cursor-pointer hover:scale-120 duration-300 ease-in-out"
+            className="p-[5px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110"
           >
             <img src={HeartIcon} alt="Add to favourite" />
           </button>
@@ -60,4 +61,4 @@ export const ArtworkCard = ({ artwork }: { artwork: Artwork }) => {
       </div>
     </div>
   );
-}
+};
