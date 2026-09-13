1d12edc43071ea305e560d7d064859d7879a2f3e
Vadym Kirilkov <tav0r0tuk@gmail.com>
2026-09-13 16:49:20 +0200

    feat(ui): create ArtworkImage component and connect to ArtworkPage

---
+++ src/components/features/ArtworkDetails/ArtworkImage.tsx
@@ -0,0 +1,51 @@
+import type { Artwork } from '@/types/artwork';
+
+import HeartIcon from '@/assets/heart-icon.svg';
+
+interface Props {
+  artwork: Artwork;
+  className?: string;
+}
+
+export const ArtworkImage = ({ artwork, className = '' }: Props) => {
+  const { title, image_url } = artwork;
+  const date = artwork.created_at
+    ? new Date(artwork.created_at).toLocaleDateString('en-GB')
+    : 'xx/xx/xxxx';
+
+  return (
+    <div
+      className={`-mx-global px-global border-b border-primary ${className}`}
+    >
+      <div className="relative h-full border-x border-primary flex items-center justify-center p-[45px]">
+        <div
+          className={`
+            absolute top-0 left-0 w-full
+            flex justify-between items-center p-[12px]
+            text-primary/50 text-[12px] font-[300] uppercase
+            pointer-events-none
+          `}
+        >
+          <span>artwork</span>
+          <span>{date}</span>
+        </div>
+
+        <img
+          src={image_url}
+          alt={title}
+          className="max-w-full max-h-full object-contain"
+        />
+
+        <div className="absolute bottom-0 right-0 flex p-[10px]">
+          <button type="button">
+            <img
+              src={HeartIcon}
+              alt="Add to favorite"
+              className="invert opacity-40 hover:opacity-100 transition-opacity"
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/ArtworkPage.tsx
+++ src/pages/ArtworkPage.tsx
@@ -4,12 +4,16 @@ import { useParams } from 'react-router-dom';
 import { useArtworkStore } from '@/store/useArtworkStore';
 import { Loader } from '@/components/ui/Loader';
 
+import { ArtworkImage } from '@/components/features/ArtworkDetails/ArtworkImage';
+
 export const ArtworkPage = () => {
   const { id } = useParams<{ id: string }>();
 
   const currentArtwork = useArtworkStore((state) => state.currentArtwork);
   const fetchArtworkById = useArtworkStore((state) => state.fetchArtworkById);
-  const clearCurrentArtwork = useArtworkStore((state) => state.clearCurrentArtwork);
+  const clearCurrentArtwork = useArtworkStore(
+    (state) => state.clearCurrentArtwork,
+  );
   const isLoading = useArtworkStore((state) => state.isLoading);
   const error = useArtworkStore((state) => state.error);
 
@@ -26,12 +30,15 @@ export const ArtworkPage = () => {
     return <p>{error || 'Artwork not found'}</p>;
   }
 
+  const { title } = currentArtwork;
+
   return (
-    <div>
-      <h1>{currentArtwork.title}</h1>
-      <p>
-        {currentArtwork.artist.first_name} {currentArtwork.artist.last_name}
-      </p>
+    <div className="px-global min-h-[calc(100svh-var(--header-height))] relative">
+      <div className="mt-[65px] pb-[24px] border-b-[1px] border-primary -mx-global px-global">
+        <h2 className="text-[52px]">{title}</h2>
+      </div>
+
+      <ArtworkImage artwork={currentArtwork} className="h-[420px]" />
     </div>
   );
 };
